b219e21361d215f0268b847ddb0c3180604eaa99
Marcus Miguel <marcusmigueell@gmail.com>
2021-05-24 11:29:18 -0300

    Exemplifying Some Events

---
+++ js/meu_script.js
@@ -0,0 +1,89 @@
+/*
+    Alguns eventos principais do Mouse
+
+    Evento onclick ------> Acionado quando o elemento é clicado
+    Evento ondblclick ---> Acionado quando o elemento é clicado 2 vezes
+    Evento onmouseup ----> Acionado quando o clique sobre o elemento é liberado
+    Evento onmouseover --> Acionado quando o cursor do mouse sobrepõe o elemento
+    Evento onmouseout ---> Acionado quando o cursor do mouse sai da região da página ocupada pelo elemento
+
+    Alguns eventos não podem ser aplicados em elementos html, para ver quais se aplicam e quais não acessar o link:
+    https://www.w3schools.com/jsref/dom_obj_event.asp
+
+
+
+    function acao() {
+        alert('Evento disparado')
+    }
+
+    function acao1() {
+        alert('Elemento foi clicado')
+    }
+
+    function acao2() {
+        alert('O cursor do mouse deixou a região do elemento')
+    }
+
+*/
+
+/*--------------------------------------------------------------------------------------------------------------------------------------*/
+
+/*
+    Alguns eventos principais do Teclado
+
+    Evento onkeydown ------> Acionado no momento em que qualquer tecla é pressionada
+    Evento onkeypress -----> Acionado quando eu mantenho qualquer tecla que seja um caracter pressionada
+    Evento onkeyup --------> Acionado no momento em que qualquer tecla é liberada
+
+    Alguns eventos não podem ser aplicados em elementos html, para ver quais se aplicam e quais não acessar o link:
+    https://www.w3schools.com/jsref/dom_obj_event.asp
+
+
+    function acao() {
+        alert('Evento disparado')
+    }
+
+*/
+
+/*--------------------------------------------------------------------------------------------------------------------------------------*/
+
+/*
+    Alguns eventos principais de Janela ou Frame, é um evento especifico do <body>
+
+    Evento onresize ---> Acionado quando a página ou frame é redimensionada 
+    Evento onscroll ---> Acionado quando o scroll do mouse é acionado, qualquer elemento que possua um scroll
+
+    Alguns eventos não podem ser aplicados em elementos html, para ver quais se aplicam e quais não acessar o link:
+    https://www.w3schools.com/jsref/dom_obj_event.asp
+
+*/
+
+/*--------------------------------------------------------------------------------------------------------------------------------------*/
+
+/*
+    Alguns eventos principais de Formulário
+
+    Evento onfocus ----> Acionado quando o elemento recebe o foco
+    Evento onblur -----> Acionado quando o elemento perde o foco
+    Evento onchange ---> Acionado quando o estado do elemento é modificado
+
+    Alguns eventos não podem ser aplicados em elementos html, para ver quais se aplicam e quais não acessar o link:
+    https://www.w3schools.com/jsref/dom_obj_event.asp
+
+
+
+    function acao1() {
+        console.log('Recebeu o foco')
+    }
+
+    function acao2() {
+        console.log('Perdeu o foco')
+    }
+
+    function acao3() {
+        console.log('Estado modificado')
+    }
+
+*/
+
+/*--------------------------------------------------------------------------------------------------------------------------------------*/
