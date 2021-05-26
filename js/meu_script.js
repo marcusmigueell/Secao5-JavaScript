3e7577b8bc4dfe29cafcf37d8e550a6041218d1a
Marcus Miguel <marcusmigueell@gmail.com>
2021-05-26 16:07:51 -0300

    BOM concepts

---
+++ js/meu_script.js
@@ -0,0 +1,14 @@
+/*
+
+    Window
+
+    var janela
+    function abrirPopUp() {
+        janela = window.open('http://www.google.com', 'nova_janela', 'width=200, height=100')
+    }
+
+    function fecharPopUp() {
+        janela.close()
+    }
+
+*/
