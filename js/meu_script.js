526872c3d281c6a994731e47b5fec6bd9f557e66
Marcus Miguel <marcusmigueell@gmail.com>
2021-05-26 17:19:03 -0300

    BOM Concepts

--- js/meu_script.js
+++ js/meu_script.js
@@ -12,3 +12,63 @@
     }
 
 */
+
+/*--------------------------------------------------------------------------------------------------------------------------------*/
+
+/*
+
+    Screen
+
+    var altura = window.screen.availHeight  // altura do navegador
+    var largura = window.screen.availWidth  // largura do navegador
+
+    document.write(altura + '<br>')
+    document.write(largura)
+
+*/
+
+/*--------------------------------------------------------------------------------------------------------------------------------*/
+
+/*
+
+    Location
+
+    window.location.href = 'http://www.google.com'
+
+*/
+
+/*--------------------------------------------------------------------------------------------------------------------------------*/
+
+/*
+
+    Timing
+
+    setTimeout(<ação>, <tempo em milissegundos>) --> Executa ação uma vez após o tempo informado
+    clearTimeout()
+
+    setInterval(<ação>, <tempo em milissegundos>) -> Sempre executa a ação após o período informado
+    setInterval()
+
+    setTimeout(function(){document.write('Teste')}, 2000)
+
+    var i = 5 
+
+    setInterval(function() {
+        document.write(i + '<br>')
+        i--
+
+        if (i ===0 ) {
+            window.location.reload()
+        }
+    }, 1000)
+
+    var x = setInterval(function() {
+        document.write(i + '<br>')
+        i--
+
+        if (i ===0 ) {
+            clearInterval(x)
+        }
+    }, 1000)
+
+*/
